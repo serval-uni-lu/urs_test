f46b1473b8595f5586811753870598706a2072c2
Olivier Zeyen <olivierzeyen@gmail.com>
2023-05-15 14:44:52 +0200

    res.csv

--- bday_sizes/run.py
+++ bday_sizes/run.py
@@ -1,5 +1,24 @@
+import math
+import numpy as np
+import decimal
 import pandas as pd
 
 mc =  pd.read_csv("data/mc.csv", skipinitialspace = True, index_col = 'file')
 
-print(mc)
+desired = 0.05
+factor = 0
+
+if desired < 1.0:
+    factor = math.sqrt(-2.01 * math.log(desired))
+else:
+    factor = math.sqrt(2.01 * desired)
+
+
+# rng_range = mc['#m'] + 0.01
+# rng_range = mc['#m']
+# sample_size = np.ceil(factor * np.sqrt(rng_range))
+
+for i in mc.index:
+    rng_range = decimal.Decimal(mc['#m'][i])
+    sample_size = factor * math.sqrt(rng_range)
+    print(f"{i}, {rng_range}, {sample_size}")
