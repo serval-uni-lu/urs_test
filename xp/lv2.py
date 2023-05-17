a5f81fe6429c342722c3039a5c51eba85374b1a2
zyno1 <olivierzeyen@gmail.com>
2023-05-17 15:46:42 +0200

    cleanup

--- xp/lv2.py
+++ xp/lv2.py
@@ -1,12 +1,13 @@
 from scipy.stats import chi2
 from scipy.stats import chisquare
 import pandas as pd
+import numpy as np
 import math
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--csv", type=str)
-parser.add_argument("-a", type=float, default=0.05)
+parser.add_argument("-a", type=float, default=0.01)
 
 args = parser.parse_args()
 
@@ -14,24 +15,39 @@ significance_level = args.a
 csv = args.csv
 
 data = pd.read_csv(csv, skipinitialspace = True, index_col = 'file')
+data.dropna(inplace = True)
 
-T = len(data[data.isUniform == True])
-F = len(data[data.isUniform == False])
-total = T + F
+T = data[data.isUniform == True]['pvalue']
+F = data[data.isUniform == False]['pvalue']
 
-Te = round((1 - significance_level) * total)
-Fe = round(significance_level * total)
+nb = len(T) + len(F)
+print(f"nb samples: {nb}")
+print(f"T: {len(T)} + F: {len(F)}")
 
+pr = 1.0 / ((1.0 / nb) * (np.sum(1.0 / T) + np.sum(1.0 / F)))
+print(f"pr: {pr}")
+print(f"res: {pr <= significance_level}")
 
-observed = [T, F]
-expected = [Te, Fe]
-
-print(f"expected: {Te}, {Fe}, ({Te + Fe})")
-print(f"observed: {T}, {F}, ({total})")
-
-X2, pv = chisquare(observed, expected)
-crit = chi2.ppf(1 - significance_level, df = 1)
-print(f"X2 {X2}")
-print(f"crit {crit}")
-print(f"pv {pv}")
-print(f"u {X2 <= crit}")
+# print("----------------------------------------")
+# 
+# a = 0.05 / nb
+# 
+# Fo = np.sum(T < a) + np.sum(F < a)
+# To = nb - Fo
+# 
+# Te = round((1 - significance_level) * nb)
+# Fe = round(significance_level * nb)
+# # 
+# # 
+# observed = [To, Fo]
+# expected = [Te, Fe]
+# # 
+# print(f"expected: {Te}, {Fe}, ({Te + Fe})")
+# print(f"observed: {To}, {Fo}, ({nb})")
+# # 
+# X2, pv = chisquare(observed, expected)
+# crit = chi2.ppf(1 - significance_level, df = 1)
+# print(f"X2 {X2}")
+# print(f"crit {crit}")
+# print(f"pv {pv}")
+# print(f"u {X2 <= crit}")
---
+++ xp/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function run {
+	r=$(./chi2.sif -c "$2" -s "$1" -a "0.0001666666666666667")
+	u=$(echo "$r" | grep -E "^u " | sed 's/^u //g')
+	pv=$(echo "$r" | grep -E "^pv " | sed 's/^pv //g')
+	echo "$2, $u, $pv"
+}
+export -f run
+
+id="4"
+n="lookahead"
+
+echo "file, isUniform, pvalue" > "$1_$n.csv"
+find "$1" -name "*.cnf" | parallel -n 1 -P 3 run "$id" >> "$1_$n.csv"
