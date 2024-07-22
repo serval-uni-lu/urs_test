6a280b932e09a2b5cf39eb4ae7a1d79ca28bbdf1
Olivier Zeyen <olivierzeyen@gmail.com>
2024-07-22 09:03:28 +0200

    fix

--- chi2/chi2.py
+++ chi2/chi2.py
@@ -518,7 +518,7 @@ def monobit():
         return
 
     sample_size = max(batch_size, math.ceil(total_mc * min_elem_per_cell / min(uneven, even)))
-    print(f"sample size: {sample_size}")
+    print(f"sample size: {math.ceil(sample_size / batch_size) * batch_size}")
     # gof = GofChisquarePower()
     # sample_size = gof.solve_power(effect_size = effect_size, nobs = None, alpha = significance_level, power = power, n_bins = 2)
 
@@ -582,7 +582,7 @@ def frequency_variables():
             m = min(m, nnf.get_node(1).mc_by_var[i])
 
     sample_size = max(batch_size, math.ceil(total_mc * min_elem_per_cell / m))
-    print(f"sample size: {sample_size}")
+    print(f"sample size: {math.ceil(sample_size / batch_size) * batch_size}")
     # gof = GofChisquarePower()
     # sample_size = gof.solve_power(effect_size = effect_size, nobs = None, alpha = significance_level, power = power, n_bins = 2)
 
@@ -684,7 +684,7 @@ def frequency_nb_variables():
             m = min(m, i)
 
     sample_size = max(batch_size, math.ceil(total_mc * min_elem_per_cell / m))
-    print(f"sample size: {sample_size}")
+    print(f"sample size: {math.ceil(sample_size / batch_size) * batch_size}")
 
     if batch_size == -1:
         batch_size = sample_size
@@ -773,7 +773,7 @@ def birthday_test():
     expected = scipy.special.binom(sample_size, 2) / rng_range
     p_zero = math.exp(-1 * expected)
 
-    print(f"sample size: {sample_size}")
+    print(f"sample size: {math.ceil(sample_size / batch_size) * batch_size}")
     print(f"expected: {expected}")
     print(f"prob of zero duplicates: {p_zero}")
 
@@ -839,7 +839,7 @@ def pearson_chisquared():
     # gof = GofChisquarePower()
     # sample_size = gof.solve_power(effect_size = effect_size, nobs = None, alpha = significance_level, power = power, n_bins = rng_range)
 
-    print(f"sample size: {sample_size}")
+    print(f"sample size: {math.ceil(sample_size / batch_size) * batch_size}")
 
     for _ in range(0, args.n):
         #samples = []
