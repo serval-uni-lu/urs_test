6769d1d820231485af0918962cf46582f33b2854
Olivier Zeyen <olivierzeyen@gmail.com>
2025-11-21 15:11:30 +0100

    gibbs splitting sampler

--- chi2/src/samplers.py
+++ chi2/src/samplers.py
@@ -526,6 +526,33 @@ def getSolutionFromMSTS(inputFile, numSolutions, newSeed):
     random.shuffle(sollist)
     return sollist
 
+def getSolutionFromGIBBS(inputFile, numSolutions, newSeed):
+
+    inputFileSuffix = inputFile.split('/')[-1][:-4]
+    # tempOutputFile = tempfile.gettempdir() + '/' + inputFileSuffix + ".txt"
+    tempOutputFile = util.make_temp_name()
+    cwd = os.getcwd()
+    cmd = f'/xor/build/gibbs --cnf {os.path.abspath(inputFile)} --n {numSolutions} --k 1 > {tempOutputFile}'
+    # if args.verbose:
+    print("cmd: ", cmd)
+    # os.chdir(str(os.getcwd()) + '/samplers')
+    os.system(cmd)
+    # os.chdir(str(cwd))
+
+    with open(tempOutputFile, 'r') as f:
+        lines = f.readlines()
+
+    os.unlink(str(tempOutputFile))
+
+    if len(lines) <= 0:
+        print(len(lines))
+        print("GIBBS did not find solutions")
+        sys.exit(1)
+
+    sollist = list(map(util.solstr_to_frozenset, lines))
+    random.shuffle(sollist)
+    return sollist
+
 def getSolutionFromDistAware(inputFile, numSolutions, newSeed):
 
     inputFileSuffix = inputFile.split('/')[-1][:-4]
@@ -690,6 +717,7 @@ def getSamplerFunction(sampler, cnf_file):
             , "bddsampler" : getSolutionFromBDDSampler
             , "rupsampler" : getSolutionFromRUPSampler
             , "msts" : getSolutionFromMSTS
+            , "gibbs" : getSolutionFromGIBBS
         }
 
     return samplers[sampler]
